b0096dad0a960e507bf990692bfcd1251aeba43f
MrGultekin <gultekin.de@gmail.com>
2022-07-26 14:31:23 +0200

    returned back to initial one

--- ratingApp/src/components/FeedbackItem.jsx
+++ ratingApp/src/components/FeedbackItem.jsx
@@ -5,20 +5,12 @@ const FeedbackItem = props => {
 	const [rating, setRating] = useState(7)
 	const [text, setText] = useState('Text from useState.')
 
-	const handleClick = () => {
-		// setRating(70)
-		setRating((prevState)=>{
-			console.log({prevState})
-		 	return prevState +1 ;
-		})
-	};
+
 
 	return (
 		<div className="card">
 			<div className="num-display">{rating}</div>
 			<div className="text-display">{text}</div>
-			<button onClick={handleClick}>Click Me!....</button>
-
 		</div>
 	);
 };
