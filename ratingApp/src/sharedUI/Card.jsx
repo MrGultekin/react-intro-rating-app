b2f04480011365121c7f4c3f38f1a1a953727907
MrGultekin <gultekin.de@gmail.com>
2022-07-27 11:13:30 +0200

    works Condition by STYLE

--- ratingApp/src/sharedUI/Card.jsx
+++ ratingApp/src/sharedUI/Card.jsx
@@ -2,14 +2,19 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 const Card = ({children,reverse}) => {
-	return (
-		<div className={`card ${reverse && 'reverse'}`}>
+	// return (<div className={`card ${reverse && 'reverse'}`}>{children}</div>);
+
+	return(
+		<div className='card'
+		     style={{
+			backgroundColor:reverse ? 'rgba(0,0,0,0.4)' : '#fff',
+			color:reverse ? '#fff' : '#000',
+		}}>
 			{children}
 		</div>
-	);
+	)
 }
-
-Card.propTypes = {};
+// Card.propTypes = {};
 
 
 export default Card;
