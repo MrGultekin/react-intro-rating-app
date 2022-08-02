dadac7907e05a1c916a4d212cf812a3ff7b1d869
MrGultekin <gultekin.de@gmail.com>
2022-08-02 16:31:51 +0200

    catch ids when x is clicked

--- ratingApp/src/components/FeedbackItem.jsx
+++ ratingApp/src/components/FeedbackItem.jsx
@@ -1,10 +1,19 @@
 import Card from "../sharedUI/Card.jsx";
 import PropTypes from "prop-types";
+// import {FaTimes} from "react-icons/all.js";
+import {FaTimes} from "react-icons/fa";
 
 const FeedbackItem = ({item}) => {
+	function handleClick(id) {
+		console.log(id)
+	}
+
 	return (
 		<Card>
 			<div className="num-display">{item.rating}</div>
+			<button className='close' onClick={()=> handleClick(item.id)}>
+				<FaTimes color={'purple'}/>
+			</button>
 			<div className="text-display">{item.text}</div>
 		</Card>
 	);
